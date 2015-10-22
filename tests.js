cc430496a06ffd60df87d45aa59b75f6be77a7ad
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-10-22 10:37:52 +0300

    added first test

--- tests.js
+++ tests.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Container from './index';
 import assert from 'assert';
 
 describe('Container', () => {
 	// TODO: describe use cases
+	const Component = (props) => <div />;
+
+	it('should create React component', () => {
+		const PContainer = Container(Component, {});
+
+		assert.ok(React.isValidElement(<PContainer />), 'Container should be a React component');
+		assert.equal(PContainer.displayName, 'ComponentContainer', 'Container should have Component\'s name with suffix');
+	});
 });
