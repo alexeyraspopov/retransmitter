804944a62f38c0ab4c50c3ca068032ff10e8d946
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-10-28 20:07:54 +0200

    used lower case for query string

--- examples/query-list/components/List.js
+++ examples/query-list/components/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from 'components/Item';
 
 export default function List({items, query = ''}) {
-	const isQueried = ({title}) => title.toLowerCase().includes(query);
+	const isQueried = ({title}) => title.toLowerCase().includes(query.toLowerCase());
 	const item = ({id, title, description}) => <Item key={id} title={title} description={description} />;
 
 	return (
