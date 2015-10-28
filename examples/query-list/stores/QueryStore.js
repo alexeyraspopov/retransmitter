f416d2e9b53f1baf70bf6f96fa560969654cc403
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-10-28 23:03:17 +0200

    removed initial state of QueryStore

--- examples/query-list/stores/QueryStore.js
+++ examples/query-list/stores/QueryStore.js
@@ -2,7 +2,7 @@ import {createStore as Store} from 'redux';
 
 export default Store(QueryState);
 
-function QueryState(query = 'l', action) {
+function QueryState(query = '', action) {
 	switch (action.type) {
 	case 'QUERY_CHANGED':
 		return action.query;
