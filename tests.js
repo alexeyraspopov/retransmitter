b884609f694a3e0713ae7a84ac464819e5c74812
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-10-28 21:31:13 +0200

    fixed tests

--- tests.js
+++ tests.js
@@ -8,27 +8,27 @@ describe('Container', () => {
 	const Component = (props) => <div />;
 
 	it('should create React component', () => {
-		const PContainer = Container(Component, {});
+		const PContainer = Container.create(Component, {});
 
 		assert.ok(TestUtils.isElement(<PContainer />), 'Container should be a React component');
 		assert.equal(PContainer.displayName, 'ComponentContainer', 'Container should have Component\'s name with suffix');
 	});
 
 	it('should create React component if enum is used', () => {
-		const PContainer = Container({success: Component}, {});
+		const PContainer = Container.create({success: Component}, {});
 
 		assert.ok(TestUtils.isElement(<PContainer />), 'Container should be a React component');
 		assert.equal(PContainer.displayName, 'ComponentContainer', 'Container should have Component\'s name with suffix');
 	});
 
 	it('should raise an error if no components are specified', () => {
-		assert.throws(() => Container({success: null}, {}), /Success, Failure and Pending should be React components/);
-		assert.throws(() => Container({}, {}), /Success component should be specified/);
+		assert.throws(() => Container.create({success: null}, {}), /Success, Failure and Pending should be React components/);
+		assert.throws(() => Container.create({}, {}), /Success component should be specified/);
 	});
 
 	it('should render Pending element by default', () => {
 		const Spinner = () => <p>Loading...</p>;
-		const PContainer = Container({success: Component, pending: Spinner}, {});
+		const PContainer = Container.create({success: Component, pending: Spinner}, {});
 		const ReactShallow = TestUtils.createRenderer();
 
 		ReactShallow.render(<PContainer />);
