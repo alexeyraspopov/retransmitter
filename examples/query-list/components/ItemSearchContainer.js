34a7d5fa0ef65f39793645db59328ec8bd28c362
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-11-01 18:26:29 +0200

    removed unnecessary redux's complexity

--- examples/query-list/components/ItemSearchContainer.js
+++ examples/query-list/components/ItemSearchContainer.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import {Provider, connect as Connect} from 'react-redux';
 import ItemSearch from 'components/ItemSearch';
+import Transmitter from '../../../index';
 import QueryStore from 'stores/QueryStore';
 import * as QueryActions from 'actions/QueryActions';
 
-const ItemSearchConnect = Connect(query => ({
-	query,
-	onChange: event => QueryStore.dispatch(QueryActions.changeQuery(event.target.value))
-}))(ItemSearch);
-
-export default function ItemSearchContainer() {
-	return (
-		<Provider store={QueryStore}>
-			<ItemSearchConnect />
-		</Provider>
-	);
-}
+export default Transmitter.create(ItemSearch, {
+	fragments: {
+		query() {
+			return Transmitter.fromStore(QueryStore);
+		},
+		onChange() {
+			return event => QueryStore.dispatch(QueryActions.changeQuery(event.target.value));
+		}
+	}
+});
