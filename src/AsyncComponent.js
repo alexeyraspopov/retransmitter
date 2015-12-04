92963892e260e95b2fd7dac65d52b4be13ca0d51
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-12-04 19:45:43 +0200

    call `onFetch` after loading is finished

--- src/AsyncComponent.js
+++ src/AsyncComponent.js
@@ -16,10 +16,14 @@ export default function AsyncComponent(asyncFunction) {
 			return {body: React.createElement('noscript')};
 		},
 
-		updateState(body) {
+		updateState(body, status) {
 			invariant(React.isValidElement(body), `The result of ${functionName} is not a React element`);
 
 			this.setState({body});
+
+			if (this.props.onFetch) {
+				this.props.onFetch(status);
+			}
 		},
 
 		observe(props) {
