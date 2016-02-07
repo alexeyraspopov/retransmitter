6a203890587e62b6f958d42e1681c4e43ca748f8
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-02-07 03:31:19 +0200

    removed unnecessary function

--- tests/AsyncComponent-test.js
+++ tests/AsyncComponent-test.js
@@ -4,16 +4,6 @@ import AsyncComponent from './src/AsyncComponent';
 import assert from 'assert';
 import sinon from 'sinon';
 
-function runAsync(block) {
-	return new Promise((resolve, reject) => setTimeout(async () => {
-		try {
-			resolve(await block());
-		} catch (error) {
-			reject(error);
-		}
-	}, 10));
-}
-
 describe('AsyncComponent', () => {
 	function Component() {
 		return <div />;
